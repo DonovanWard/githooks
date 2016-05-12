0afc8151071746952d59ab09e6921fd3bec46427
WardDo <donovan.b.ward@espn.com>
2016-05-12 08:54:28 -0400

    created junit test suite runner

---
+++ githooks/src/test/java/shapes/RunTestSuite.java
@@ -0,0 +1,11 @@
+package shapes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestCircle.class,TestRectangle.class,TestTriangle.class})
+public class RunTestSuite {
+
+}
